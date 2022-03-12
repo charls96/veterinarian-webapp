9e6efe01da542f5d36d406dc5db2926cda91b405
carlosdaniel <carlosdaniel@pop-os.localdomain>
2022-03-12 23:09:09 +0100

    Project Initialized and Header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
+import PatientsList from "./components/PatientsList";
+
+function App() {
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <Form />
+      <PatientsList />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/PatientsList.jsx
@@ -0,0 +1,9 @@
+const PatientsList = () => {
+  return (
+    <div>
+      <h2>PatientsList</h2>
+    </div>
+  );
+};
+
+export default PatientsList;
