9fddc68c3912e23e0cb24039f1416b8659b64443
carlosdaniel <carlosdaniel@pop-os.localdomain>
2022-03-12 23:59:40 +0100

    added patients list

---
+++ src/components/Patients.jsx
@@ -0,0 +1,33 @@
+const Patients = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Name: {""}
+        <span className="font-normal normal-case">Greta</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Owner: {""}
+        <span className="font-normal normal-case">Carlos</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">email@email.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Register date: {""}
+        <span className="font-normal normal-case">10th December 2022</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        symptoms: {""}
+        <span className="font-normal normal-case">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic delectus,
+          placeat laboriosam repudiandae quas, officiis iure voluptas deserunt
+          fuga doloremque animi! Omnis exercitationem cumque ipsa, ipsum
+          consequuntur aliquam tempore est.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Patients;
--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,7 +1,21 @@
+import Patients from "./Patients";
+
 const PatientsList = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
-      <h2>PatientsList</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="text-3xl text-center font-black">Patients List</h2>
+      <p className="text-lg text-center mt-5 mb-10">
+        Manage your{" "}
+        <span className="text-indigo-600 font-bold">Patients and Dates</span>
+      </p>
+
+      <Patients />
+      <Patients />
+      <Patients />
+      <Patients />
+      <Patients />
+      <Patients />
+      <Patients />
     </div>
   );
 };
